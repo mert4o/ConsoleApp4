6921b9dcdf861b5107a95ffd6532f6fe1dc30f0f
yodatime <electriclikethor@gmail.com>
2025-02-13 17:20:43 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static int SumOfDigits(int n)
+    {
+        if (n == 0)
+            return 0;
+        return (n % 10) + SumOfDigits(n / 10);
+    }
+
+    static void Main()
+    {
+        Console.Write("Въведете число: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("Сумата на цифрите е: " + SumOfDigits(Math.Abs(number)));
+    }
+}
